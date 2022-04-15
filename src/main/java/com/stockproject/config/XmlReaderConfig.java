6b9f21811cde7b1defc3c103b6a50dabe28144fd
Administrator <test@test.ro>
2022-04-15 10:58:05 +0300

    added path to application preperties

--- src/main/java/com/stockproject/config/XmlReaderConfig.java
+++ src/main/java/com/stockproject/config/XmlReaderConfig.java
@@ -2,6 +2,7 @@ package com.stockproject.config;
 
 
 import com.stockproject.dto.ProduseListDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +10,13 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-
+@Slf4j
 @Configuration
 @ConfigurationProperties(prefix = "path")
 public class XmlReaderConfig {
@@ -24,8 +26,20 @@ public class XmlReaderConfig {
     private String target;
     @Bean
         public ProduseListDto unmarshall() throws JAXBException, IOException {
-            JAXBContext context = JAXBContext.newInstance(ProduseListDto.class);
-        FileReader file = new FileReader(source);
+        JAXBContext context = null;
+        try {
+            context = JAXBContext.newInstance(ProduseListDto.class);
+        } catch (JAXBException e) {
+            log.error("Context load failed");
+            e.printStackTrace();
+        }
+        FileReader file = null;
+        try {
+            file = new FileReader(source);
+        } catch (FileNotFoundException e) {
+            log.error("File not found file path :" + source);
+            e.printStackTrace();
+        }
         ProduseListDto list = (ProduseListDto) context.createUnmarshaller()
                 .unmarshal(file);
         Files.createDirectories(Paths.get(target));
